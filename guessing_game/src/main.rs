241a8a268dafafb4a167d68e9283fa81b03961c5
Maciej Bratko <157412862+MaciejBratko@users.noreply.github.com>
2025-01-15 14:40:05 +0000

    modularization, loop change

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,39 +2,32 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io::{self, Write};
 
+fn get_user_guess() -> i32 {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to fetch the line.");
+    guess.trim().parse().expect("Please enter a valid number!")
+}
+
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut tries = 0;
     println!("Guess the number!");
 
-    let mut win = false;
-
-    while !win {
+    loop {
         print!("Your guess: ");
         io::stdout().flush().unwrap();
 
-        let mut guess = String::new();
-
         tries += 1;
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to fetch the line.");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number!");
-                continue;
-            }
-        };
+        let guess = get_user_guess();
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too low! Try again!\n"),
             Ordering::Greater => println!("Too high! Try again!\n"),
             Ordering::Equal => {
                 println!("\nYou win! It took you {tries} tries to guess the number!");
-                win = true;
+                break;
             }
         }
     }
